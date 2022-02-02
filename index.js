712bcd45d0220f4e3c9701c2816b5bc85a82317a
Sushmitha-94 <st.sushmi@gmail.com>
2022-02-01 21:33:13 -0600

    Final code

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//connect to database
+connectDB();
+
 app.use(express.json({ extended: false}));
 
-const PORT = 5000;
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
